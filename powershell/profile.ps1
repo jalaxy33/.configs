343727d3a2342dc15ab0c79b97aa7ed7831d68f1
jalaxy33 <jalaxy1996@outlook.com>
2025-07-28 13:57:43 +0800

    add powershell profile.ps1

---
+++ powershell/profile.ps1
@@ -0,0 +1,78 @@
+# Profile.ps1 or Microsoft.PowerShell_profile.ps1
+
+function Command-Exist {
+    param(
+        [string]$name
+    )
+    Get-Command $name -ErrorAction SilentlyContinue
+}
+
+
+## --- start apps ---
+
+if (Command-Exist starship) { Invoke-Expression (&starship init powershell) }
+if (Command-Exist zoxide) { Invoke-Expression (& { (zoxide init powershell | Out-String) }) }
+if (Command-Exist scoop) {
+    # replace default scoop search
+    if (Command-Exist scoop-search) { Invoke-Expression (&scoop-search --hook) }
+}
+if (Command-Exist uv) {
+    (& uv --generate-shell-completion powershell) | Out-String | Invoke-Expression
+    (& uvx --generate-shell-completion powershell) | Out-String | Invoke-Expression
+}
+
+
+
+## --- alias --- 
+
+# prettier ls
+if (Command-Exist eza) {
+    function eza-ls { eza --icons --git @args }
+    Set-Alias -Name ls -Value eza-ls -Option AllScope
+}
+
+function ll { ls -l @args }
+
+# smarter cd
+if (Command-Exist zoxide) { Set-Alias -Name cd -Value z -Option AllScope }
+
+
+## --- set proxy ---
+
+function set_proxy() {
+    $proxy_url = "127.0.0.1:7897"
+    $http_proxy = "http://127.0.0.1:7897"
+
+    $env:HTTP_PROXY = $http_proxy
+    $env:HTTPS_PROXY = $http_proxy
+
+    git config --global https.proxy $http_proxy
+    git config --global https.proxy $http_proxy
+
+    if (Command-Exist scoop) {
+        scoop config proxy $proxy_url
+    }
+}
+
+function unset_proxy {
+    if (Test-Path Env:HTTP_PROXY) {
+        Remove-Item Env:HTTP_PROXY
+        Remove-Item Env:HTTPS_PROXY
+    }
+
+    git config --global --unset http.proxy
+    git config --global --unset https.proxy
+
+    if (Command-Exist scoop) {
+        scoop config rm proxy
+    }
+}
+
+
+## --- environment variables ---
+
+# Rust
+$env:RUSTUP_DIST_SERVER = "https://mirrors.ustc.edu.cn/rust-static"
+$env:RUSTUP_UPDATE_ROOT = "https://mirrors.ustc.edu.cn/rust-static/rustup"
+
+
