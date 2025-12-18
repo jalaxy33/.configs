694f4f06c2d1af761962c31f30bab96c4c10fae6
jalaxy33 <41473863+jalaxy33@users.noreply.github.com>
2025-12-18 10:41:10 +0800

    update config.fish

--- fish/config.fish
+++ fish/config.fish
@@ -31,13 +31,26 @@ end
 
 
 function set_proxy
-    set -x HTTP_PROXY "http://127.0.0.1:7897"
-    set -x HTTPS_PROXY "http://127.0.0.1:7897"
+    set proxy_url "127.0.0.1:7890"
+    set http_proxy "http://$proxy_url"
+
+    echo "proxy_url: $proxy_url"
+
+    set -x ALL_PROXY $http_proxy
+    set -x HTTP_PROXY $http_proxy
+    set -x HTTPS_PROXY $http_proxy
+
+    git config --global http.proxy $http_proxy
+    git config --global https.proxy $http_proxy
 end
 
 function unset_proxy
+    set -u ALL_PROXY
     set -u HTTP_PROXY
     set -u HTTPS_PROXY
+
+    git config --global --unset http.proxy
+    git config --global --unset https.proxy
 end
 
 
