36c503660c744fc4510fef34a95d6bca1f02a241
jalaxy33 <41473863+jalaxy33@users.noreply.github.com>
2025-12-18 10:42:53 +0800

    Add proxy configuration functions
    Added functions to set and unset proxy configurations for git.

--- fish/minimal-config.fish
+++ fish/minimal-config.fish
@@ -2,13 +2,13 @@
 
 if status is-interactive
     # Commands to run in interactive sessions can go here
-    eval "$(/home/linuxbrew/.linuxbrew/bin/brew shellenv)"
     starship init fish | source
     zoxide init fish | source
     fzf --fish | source
 
     # aliasing
     alias vi="vim"
+    alias hx="helix"
     alias vifish="vim ~/.config/fish/config.fish"
     alias hxfish="hx ~/.config/fish/config.fish"
     alias catfish="cat ~/.config/fish/config.fish"
@@ -16,14 +16,32 @@ if status is-interactive
     alias ls="eza --icons --git -a"
     alias cd="z"
 
-    # homebrew
-    set -x HOMEBREW_BREW_GIT_REMOTE "https://mirrors.ustc.edu.cn/brew.git"
-    set -x HOMEBREW_CORE_GIT_REMOTE "https://mirrors.ustc.edu.cn/homebrew-core.git"
-    set -x HOMEBREW_BOTTLE_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles"
-    set -x HOMEBREW_API_DOMAIN "https://mirrors.ustc.edu.cn/homebrew-bottles/api"
-
     # config rust
     set -x RUSTUP_DIST_SERVER "https://mirrors.ustc.edu.cn/rust-static"
     set -x RUSTUP_UPDATE_ROOT "https://mirrors.ustc.edu.cn/rust-static/rustup"
 
 end
+
+function set_proxy
+    set proxy_url "127.0.0.1:7890"
+    set http_proxy "http://$proxy_url"
+
+    echo "proxy_url: $proxy_url"
+
+    set -x ALL_PROXY $http_proxy
+    set -x HTTP_PROXY $http_proxy
+    set -x HTTPS_PROXY $http_proxy
+
+    git config --global http.proxy $http_proxy
+    git config --global https.proxy $http_proxy
+end
+
+function unset_proxy
+    set -u ALL_PROXY
+    set -u HTTP_PROXY
+    set -u HTTPS_PROXY
+
+    git config --global --unset http.proxy
+    git config --global --unset https.proxy
+end
+
