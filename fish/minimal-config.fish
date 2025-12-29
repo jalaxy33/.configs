ff3bc0a720e02c021c5b084d6b0dbd3bd989c3f0
jalaxy33 <41473863+jalaxy33@users.noreply.github.com>
2025-12-29 10:23:36 +0800

    Update minimal-config.fish

--- fish/minimal-config.fish
+++ fish/minimal-config.fish
@@ -1,6 +1,8 @@
 # ~/.config/fish/config.fish
 
 if status is-interactive
+    set fish_greeting ""
+
     # Commands to run in interactive sessions can go here
     starship init fish | source
     zoxide init fish | source
